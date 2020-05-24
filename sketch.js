62558bd7e27779f29f9cdbd42ed7c730a3cd3016
Arushmal <65401185+Arushmal@users.noreply.github.com>
2020-05-24 16:26:18 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,29 @@
-const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
 
-var engine, world;
-var box1;
-var box2;
-var Ground;
+
+var supply;
+var helicopter;
+var bucket;
 
 function setup(){
     var canvas = createCanvas(400,400);
-    engine = Engine.create();
-    world = engine.world;
+   helicopter=createSprite(350,200,50,20);
+   helicopter.velocityX=2;
+   helicopter.Animation("helicopter.png");
+ 
 
-    box1 = new Box(200,300,50,50);
-    box2= new Box(240,100,60,60);
-    Ground=new ground(200,390,400,20);
+   supply=createSprite(350,200,10,10);
+supply.velocityX=helicopter.velocityX;
+supply.Animation("package.png");
+   
 }
 
 function draw(){
     background(0);
-    Engine.update(engine);
-    box1.display();
-    box2.display();
-    Ground.display();
    
+
+   if(keyDown("DOWN_ARROW"))
+{
+   supply.velocityY=-2;
+}
+
 }
